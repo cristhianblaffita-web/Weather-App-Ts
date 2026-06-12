9e6dcef926e5249f9f50470b2b4a48e5a9a91a5c
cristhianblaffita-web <cristhianblaffita@gmail.com>
2026-06-12 12:33:10 -0400

    feature: Add CityResults to display search results while consuming WeatherContext data.

--- src/components/WeatherNav.tsx
+++ src/components/WeatherNav.tsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
+import { useWeatherContext } from "../context/WeatherContext";
+import type { Place } from "../hooks/useGeocoding";
 import { getWeatherIcon } from "../utils/weatherCodes";
-import { Search } from "lucide-react";
+import { MapIcon, Search } from "lucide-react";
 
 type WeatherNavProps = {
     city?: string;
@@ -7,6 +10,26 @@ type WeatherNavProps = {
     date?: string;
     hour?: string;
     isDay?: boolean;
+}
+
+export function CityResults({results, handleSelect}:{results: Place[]; handleSelect: (city: Place) => void}) {
+    if (!results || !results.length) return null;
+
+    return (
+        <ul className="absolute left-0 right-0 top-10 bg-card">
+            {results.map(city => (
+                <li
+                    key={city.id}
+                    className="flex overflow-auto gap-2 text-text-secondary cursor-pointer px-4 py-2 hover:bg-accent/20"
+                    onMouseDown={() => handleSelect(city)}
+                >
+                    <MapIcon/>
+                    <span>{city.country},</span>
+                    <span className="text-nowrap">{city.name}</span>
+                </li>
+            ))}
+        </ul>
+    )
 }
 
 export default function WeatherNav({
@@ -17,6 +40,12 @@ export default function WeatherNav({
     isDay = true
 }: WeatherNavProps) {
     const Icon = getWeatherIcon(code, isDay);
+
+    const { search, setSearch, cities, handleSelect, loading } = useWeatherContext();
+
+    const [inputActive, setInputActive] = useState(false);
+
+    const showResults = inputActive && search.length > 1 && !!cities?.results?.length;
 
     return (
         <div className="weather-nav bg-card-elevated/20 grid grid-cols-[auto_1fr] grid-rows-2 items-center gap-y-4 gap-x-4 rounded-2xl border border-border/40 p-4 shadow-sm">
@@ -33,13 +62,19 @@ export default function WeatherNav({
                 <input
                     className="focus:outline-0 text-sm text-text-secondary"
                     type="search"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    onFocus={() => setInputActive(true)}
+                    onBlur={() => setInputActive(false)}
                     placeholder="Search city..."
                     minLength={2}
                     maxLength={30}
                     aria-label="Search city"
                 />
                
+               {showResults && <CityResults results={cities.results} handleSelect={handleSelect} />}
             </div>
+            {loading && <p>Loading</p>}
         </div>
     )
 }
